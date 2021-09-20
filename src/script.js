1fe4fcf70d9366868db71db82af34a136568f86f
daniel-walters <waltersdaniel96@gmail.com>
2021-09-20 13:52:39 +0800

    search bar now makes request to API and logs response

--- src/script.js
+++ src/script.js
@@ -1,9 +1,17 @@
+const COCKTAIL_BASE_API = "https://www.thecocktaildb.com/api/json/v1/1"
+
 //set click listener on search button and prevent page refresh
-document.getElementById("search-form").addEventListener("click", (event) => {
+document.getElementById("search-form").addEventListener("submit", (event) => {
     event.preventDefault();
     const searchBar = document.getElementById("search-bar");
-    console.log(searchBar.value); //log out API response instead once integrated.
-    searchBar.value = ""; //clear search bar
+    //make get request for search term, log the response then clear the search bar
+    if (searchBar.value) {
+        fetch(`${COCKTAIL_BASE_API}/search.php?s=${searchBar.value.toLowerCase()}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error))
+            .finally(() => searchBar.value = "");
+    }
 });
 
 //set click listener on recommend drink button
